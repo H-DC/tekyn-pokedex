cc4e302e7f21359d34df1f959857f05a936c6e58
Hubert de Carcouët <carcoueth@gmail.com>
2022-03-24 19:27:34 +0100

    fix: layout styles changes

--- src/components/styles/layouts/DefaultLayout.js
+++ src/components/styles/layouts/DefaultLayout.js
@@ -7,6 +7,7 @@ export const DefaultLayout = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding: 5rem;
     background: ${({ theme }) =>
         `linear-gradient(${theme.colors.primary || '#f4a261'}, ${
             theme.colors.secondary || '#141a2b'
--- src/components/styles/navs/NavBar.js
+++ src/components/styles/navs/NavBar.js
@@ -3,19 +3,21 @@ import styled from '@emotion/styled'
 export const NavBar = styled.div`
     position: relative;
     width: 100%;
-    height: 3rem;
     padding: 1rem 0rem;
     display: flex;
     align-items: center;
     justify-content: center;
     border-bottom: 1px solid ${({ theme }) => theme.colors.primary};
 
+    & h1 {
+        margin: 0;
+    }
+
     & > .navbar__logo {
         position: absolute;
         display: flex;
         align-items: center;
         justify-content: center;
-        height: 3rem;
         padding: 1rem;
         top: 0;
         left: 0;
