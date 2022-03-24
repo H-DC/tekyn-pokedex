81d578ef65d2c46b28d5392a63756fd8c19f6841
Hubert de Carcouët <carcoueth@gmail.com>
2022-03-24 19:22:09 +0100

    fix: box sizing and colors name

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const theme = {
     colors: {
         primary: '#f4a261',
         secondary: '#141a2b',
+        info: '#E76F51',
+        active: '#38A8D5',
 
         normal: '#a8a87b',
         fire: '#ee803b',
@@ -12,10 +14,10 @@ const theme = {
         grass: '#7bc757',
         electric: '#f7cf43',
         ice: '#9ad8d8',
-        fight: '#be322e',
+        fighting: '#be322e',
         poison: '#9b4a99',
         ground: '#dfbf6e',
-        flight: '#a893ed',
+        flying: '#a893ed',
         psychic: '#ec5b89',
         bug: '#a8b732',
         rock: '#b89f41',
@@ -30,9 +32,11 @@ const theme = {
 const globalStyles = {
     body: {
         margin: 0,
-        boxSizing: 'border-box',
         fontFamily:
             "'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+    },
+    '*': {
+        boxSizing: 'border-box',
     },
 }
 
